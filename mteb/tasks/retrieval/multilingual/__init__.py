e8484256c0fce001fb2803d800803562475a6efd
Lucas Georges Gabriel Charpentier Pacheco <42776846+lgcharpe@users.noreply.github.com>
2026-08-07 13:22:08 +0200

    Update __init__.py

--- mteb/tasks/retrieval/multilingual/__init__.py
+++ mteb/tasks/retrieval/multilingual/__init__.py
@@ -78,6 +78,21 @@ from .mkqa_retrieval import MKQARetrieval
 from .mlqa_retrieval import MLQARetrieval
 from .mr_tidy_retrieval import MrTidyRetrieval
 from .multi_long_doc_retrieval import MultiLongDocRetrieval
+from .nanobeir_multilingual import (
+    MultilingualNanoArguAnaRetrieval,
+    MultilingualNanoClimateFeverRetrieval,
+    MultilingualNanoDBPediaRetrieval,
+    MultilingualNanoFEVERRetrieval,
+    MultilingualNanoFiQA2018Retrieval,
+    MultilingualNanoHotpotQARetrieval,
+    MultilingualNanoMSMARCORetrieval,
+    MultilingualNanoNFCorpusRetrieval,
+    MultilingualNanoNQRetrieval,
+    MultilingualNanoQuoraRetrieval,
+    MultilingualNanoSCIDOCSRetrieval,
+    MultilingualNanoSciFactRetrieval,
+    MultilingualNanoTouche2020Retrieval,
+)
 from .neu_clir2022_retrieval import (
     NeuCLIR2022Retrieval,
     NeuCLIR2022RetrievalHardNegatives,
@@ -189,6 +204,19 @@ __all__ = [
     "MintakaRetrieval",
     "MrTidyRetrieval",
     "MultiLongDocRetrieval",
+    "MultilingualNanoArguAnaRetrieval",
+    "MultilingualNanoClimateFeverRetrieval",
+    "MultilingualNanoDBPediaRetrieval",
+    "MultilingualNanoFEVERRetrieval",
+    "MultilingualNanoFiQA2018Retrieval",
+    "MultilingualNanoHotpotQARetrieval",
+    "MultilingualNanoMSMARCORetrieval",
+    "MultilingualNanoNFCorpusRetrieval",
+    "MultilingualNanoNQRetrieval",
+    "MultilingualNanoQuoraRetrieval",
+    "MultilingualNanoSCIDOCSRetrieval",
+    "MultilingualNanoSciFactRetrieval",
+    "MultilingualNanoTouche2020Retrieval",
     "NeuCLIR2022Retrieval",
     "NeuCLIR2022RetrievalHardNegatives",
     "NeuCLIR2023Retrieval",
